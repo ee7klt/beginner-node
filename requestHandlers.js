8d54b8b853cc2f157539d59969a3e38355405488
ee7klt <kltah1@gmail.com>
2013-08-29 11:54:42 +0800

    added blocking sleep() to requestHandlers

--- requestHandlers.js
+++ requestHandlers.js
@@ -3,10 +3,18 @@
 // handle objects defined in index.js
 // passed as collection of objects to route() in server.js
 // 28 Aug 2013: added return values to functions (handling requests) -- how not to do it
+// 29 Aug 2013: added sleep() - blocking
 
 
 function start() {
 	console.log("Request handler 'start' was called.");
+
+	function sleep(milliSeconds) {
+		var startTime = new Date().getTime();
+		while (new Date().getTime() < startTime + milliSeconds);    // do nothing
+	}
+
+	sleep(10000);   
 	return "Hello Start";
 }
 
