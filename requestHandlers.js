61b7fb0c64cdcfe5fa8b9ed9d5a7622476b2a1f7
ee7klt <kltah1@gmail.com>
2013-09-02 12:47:35 +0800

    requestHandlers.js: upload() uses request object and node-formidable to save uploaded file to /tmp/test.png

--- requestHandlers.js
+++ requestHandlers.js
@@ -18,8 +18,9 @@
 
 var querystring = require("querystring");
 	fs = require("fs");
+	formidable = require("formidable");
 
-function start(response, postData) {
+function start(response) {
 	console.log("Request handler 'start' was called.");
 
 
@@ -45,17 +46,32 @@ function start(response, postData) {
   
 }
 
-function upload(response, postData) {
-	console.log("Request hander 'upload' was called.");
-	response.writeHead(200, {"Content-Type": "text/plain"});
-	response.write("You've sent: " + querystring.parse(postData).text);
+function upload(response, request) {
+	console.log("Request handler 'upload' was called.");
+
+	var form = new formidable.IncomingForm();
+	console.log("about to parse");
+
+	form.parse(request, function(error, fields,files) {
+		console.log("parsing done");
+
+
+	fs.rename(files.upload.path, "/tmp/test.png", function (error) {
+		if (error) {
+			fs.unlink("/tmp/test.png");
+			fs.rename(files.upload.path, "tmp/test.png");
+		}
+	});
+	response.writeHead(200, {"Content-Type": "text/html"});
+	response.write("received image: <br/>");
+	response.write("<img src='/show' />");
 	response.end();
-	//return "Hello Upload";
+	});
 }
 
-function show(response, postData) {
+function show(response) {
 	console.log("Request handler 'show' was called.");
-	fs.readFile("tmp/test.png", "binary", function(error, file) {
+	fs.readFile("/tmp/test.png", "binary", function(error, file) {
 		if (error) {
 			response.writeHead(500, {"Content-Type": "text/plain"});
 			response.write(error + "\n");
